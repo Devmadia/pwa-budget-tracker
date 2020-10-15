ff40c41185c2b70ab32ad6e4cae1eaebe290249f
Devmadia <agathaphan@icloud.com>
2020-10-15 00:36:45 -0500

    fixed app errors; reworked code; successful test completed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,77 +1,61 @@
+const APP_PREFIX = 'budget-tracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/assets/css/styles.css",
-    "/assets/js/index.js",
-    "/assets/js/db.js",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
-  ];
+  "./index.html",
+  "./js/index.js",
+  "./js/ibd.js",
+  "./css/styles.css",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+]
   
-  const CACHE_NAME = "static-cache-v1";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
-  self.addEventListener("install", (evt) => {
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+      caches.match(e.request).then(function (request) {
+          if (request) {
+              console.log('responding with cache : ' + e.request.url)
+              return request
+          } else {
+              console.log('file is not cached, fetching : ' + e.request.url)
+              return fetch(e.request)
+          }
       })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", (evt) => {
-    // remove old caches
-    evt.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              return caches.delete(key);
-            }
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+          console.log('installing cache : ' + CACHE_NAME)
+          return cache.addAll(FILES_TO_CACHE)
+      })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+      caches.keys().then(function (keyList) {
+          let cacheKeeplist = keyList.filter(function (key) {
+              return key.indexOf(APP_PREFIX);
           })
-        );
+          cacheKeeplist.push(CACHE_NAME);
+
+          return Promise.all(keyList.map(function (key, i) {
+              if (cacheKeeplist.indexOf(key) === -1) {
+                  console.log('deleting cache : ' + keyList[i]);
+                  return caches.delete(keyList[i]);
+              }
+          }));
       })
-    );
-  
-    self.clients.claim();
-  });
-  
-  self.addEventListener("fetch", (evt) => {
-    //GET requests to the API
-    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
-      evt.respondWith(
-        caches
-          .open(DATA_CACHE_NAME)
-          .then((cache) => {
-            return fetch(evt.request)
-              .then((response) => {
-                // Watch response if ok, store it in the cache.
-                if (response.status === 200) {
-                  cache.put(evt.request, response.clone());
-                }
-  
-                return response;
-              })
-              .catch(() => {
-                // Network request failed, try to get it from the cache.
-                return cache.match(evt.request);
-              });
-          })
-          .catch((err) => console.log(err))
-      );
-  
-      // stop execution of the fetch event callback
-      return;
-    }
-  
-    // "offline-first" approach.
-    evt.respondWith(
-      caches.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      })
-    );
-  });
+  );
+});
